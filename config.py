6dbc07f820c2b30a870c2a1fe1a246b2988489af
bhaveshgit <bhavesh089git@gmail.com>
2020-08-18 12:46:12 +0530

    done

--- config.py
+++
@@ -1,8 +0,0 @@
-Lazada_setup = {
-    'apiService': 'https://api.lazada.sg/rest',
-    'authService': 'https://auth.lazada.com/rest',
-    'appKey': '121585',
-    'appSecret': 'dzFd2J1lCBbE3vs2I7l9LsytrP23OroD',
-
-}
-
